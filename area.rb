20ffee5f738394e81eb0de01612bbdb930ce7c1a
Peter Jang <peter.x.jang@gmail.com>
2016-01-01 11:41:45 -0600

    removed solution

--- area.rb
+++ area.rb
@@ -1,17 +1,7 @@
 # Create a Rectangle class with readable width and height attributes
 # and a method to calculate its area.
-class Rectangle
-  attr_reader :width, :height
 
-  def initialize(input_options)
-    @width = input_options[:width]
-    @height = input_options[:height]
-  end
 
-  def area
-    @width * @height
-  end
-end
 
 
 # Driver code
